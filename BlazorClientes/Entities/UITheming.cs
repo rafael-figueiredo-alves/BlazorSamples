91cb978db61f6ac901d4571ee8450dcc9b6f3716
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2023-07-25 21:48:44 -0300

    Update July 25, 2023
    => Improved Theming class and functionality, separating functions into small functions

--- BlazorClientes/Entities/UITheming.cs
+++ BlazorClientes/Entities/UITheming.cs
@@ -6,5 +6,32 @@
         public string DataBsTheme { get; set; } = "light";
         public string ColorFontStyle { get; set; } = "black";
         public bool IsDark { get; set; }
+
+        public static UITheming SetDarkMode(bool _isDark = false)
+        { 
+            if (_isDark)
+            {
+                return UITheming.SetDarkTheme();
+            }
+            else
+            {
+                return UITheming.SetLightTheme();
+            }
+        }
+
+        public static UITheming SetLightTheme()
+        {
+            return new UITheming();
+        }
+        public static UITheming SetDarkTheme() 
+        {
+            return new UITheming()
+            {
+                ColorFontStyle = "white",
+                ColorClass = "bg-dark",
+                DataBsTheme = "dark",
+                IsDark = true
+            };
+        }
     }
 }
