c5e65e31476d3c84794fe9a0eca0464bca04237a
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2023-09-08 00:15:00 -0300

    Update September 8, 2023
    => Designing Profile page

--- BlazorClientes/Pages/Profile.razor.cs
+++ BlazorClientes/Pages/Profile.razor.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Components;
+using BlazorClientes.Entities;
 
 namespace BlazorClientes.Pages
 {
     public class ProfileBase : ComponentBase
     {
+        [CascadingParameter] public UITheming? Theme { get; set; }
     }
 }
