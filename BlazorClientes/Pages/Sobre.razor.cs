4993f5f79f4e0045e8723a91390cb3ba5a3ff9c2
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2023-09-13 22:57:38 -0300

    Update September 13, 2023
    => Finished implementing About page

---
+++ BlazorClientes/Pages/Sobre.razor.cs
@@ -0,0 +1,10 @@
+﻿using BlazorClientes.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorClientes.Pages
+{
+    public class SobreBase : ComponentBase
+    {
+        [CascadingParameter] protected UITheming? Theme { get; set; }
+    }
+}
