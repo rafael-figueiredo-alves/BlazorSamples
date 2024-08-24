32cb1a124e39c53c36998436882a596018202857
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-08-23 22:02:00 -0300

    Working on RateLimitMiddleware

---
+++ BlazorClientes.Shared/Entities/DadosConsumo.cs
@@ -0,0 +1,28 @@
+﻿namespace BlazorClientes.Shared.Entities
+{
+    public class DadosConsumo
+    {
+        public DateTime UltimaResposta { get; private set; }
+        public int QtdRequesicoes { get; private set; }
+
+        public DadosConsumo(DateTime ultimaResposta, int qtdRequesicoes)
+        {
+            UltimaResposta = ultimaResposta;
+            QtdRequesicoes = qtdRequesicoes;
+        }
+
+        public bool AtingiuConsumo(int TempoEmSegundos, int RequesicoesMax)
+            => DateTime.UtcNow < UltimaResposta.AddSeconds(TempoEmSegundos) && QtdRequesicoes == RequesicoesMax;
+
+        public void IncrementarRequesicoes(int RequesicoesMax)
+        {
+            UltimaResposta = DateTime.UtcNow;
+
+            if (QtdRequesicoes == RequesicoesMax)
+                QtdRequesicoes = 1;
+
+            else
+                QtdRequesicoes++;
+        }
+    }
+}
--- WebApiClientes/Middlewares/RateLimitMiddleware.cs
+++ WebApiClientes/Middlewares/RateLimitMiddleware.cs
@@ -3,6 +3,7 @@ using WebApiClientes.Services;
 using Microsoft.Extensions.Caching.Distributed;
 using System.Text;
 using System.Text.Json;
+using BlazorClientes.Shared.Entities
 
 namespace WebApiClientes.Middlewares
 {
@@ -22,55 +23,18 @@ namespace WebApiClientes.Middlewares
 
         public async Task InvokeAsync(HttpContext context)
         {
-            if (!context.Request.Headers.TryGetValue(APIKEY, out var key))
+            if (context.Request.Headers.TryGetValue(APIKEY, out var key))
             {
-                context.Response.StatusCode = 401;
-                context.Response.ContentType = "application/json";
-                await context.Response.WriteAsync(JsonSerializer.Serialize(new Error("Não foi fornecida uma chave de API válida!")), Encoding.UTF8);
-                return;
-            }
+                var Limite = await fapiKeys.GetRequestLimit(key);
 
-            if (!await fapiKeys.isValidApiKey(key))
-            {
-                context.Response.StatusCode = 401;
-                context.Response.ContentType = "application/json";
-                await context.Response.WriteAsync(JsonSerializer.Serialize(new Error("Foi fornecida uma chave de API inválida!")), Encoding.UTF8);
-                return;
-            }
-
-            if(await fapiKeys.GetRequestLimit(key) == 0)
-            {
-                //Não há limite de requesições por segundo
-                await _next(context);
-                return;
+                if (await fapiKeys.GetRequestLimit(key) == 0)
+                {
+                    //Não há limite de requesições por segundo
+                    await _next(context);
+                    return;
+                }
             }
         }
 
-    }
-
-    public class DadosConsumo
-    {
-        public DateTime LastResponse { get; private set; }
-        public int NumberOfRequests { get; private set; }
-
-        public DadosConsumo(DateTime lastResponse, int numberOfRequests)
-        {
-            LastResponse = lastResponse;
-            NumberOfRequests = numberOfRequests;
-        }
-
-        public bool HasConsumedAllRequests(int timeWindowInSeconds, int maxRequests)
-            => DateTime.UtcNow < LastResponse.AddSeconds(timeWindowInSeconds) && NumberOfRequests == maxRequests;
-
-        public void IncreaseRequests(int maxRequests)
-        {
-            LastResponse = DateTime.UtcNow;
-
-            if (NumberOfRequests == maxRequests)
-                NumberOfRequests = 1;
-
-            else
-                NumberOfRequests++;
-        }
     }
 }
