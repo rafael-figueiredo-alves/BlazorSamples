c26c3a72a2dd0dfbfc2ca1210fb75c85dea2e343
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-08-27 21:58:07 -0300

    Update Program.cs

--- BlazorClientes/Program.cs
+++ BlazorClientes/Program.cs
@@ -6,12 +6,25 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.Toast;
 using BlazorClientes.Services.Interfaces;
+using System.Buffers.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7235/") }); //builder.HostEnvironment.BaseAddress
+builder.Services.AddScoped(sp =>
+{
+    byte[] textBytes = System.Text.Encoding.UTF8.GetBytes("cea7391443541fc0dabe35e2625c2530");
+    string teste = Convert.ToBase64String(textBytes);
+    var http = new HttpClient
+    {
+        BaseAddress = new Uri("https://localhost:7235/")
+    };
+    http.DefaultRequestHeaders.Add("API-Key", "cea7391443541fc0dabe35e2625c2530");
+    return http;
+ }); //builder.HostEnvironment.BaseAddress
+
 
 builder.Services.AddScoped<IAuthServices, AuthServices>();
 
